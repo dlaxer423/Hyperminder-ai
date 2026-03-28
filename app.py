a7ab39dd9a559e49de0d81f4922ac49cf21666c7
dlaxer423 <omerkostekci55@gmail.com>
2026-03-28 22:43:07 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+AI Kod Yazıcı
+Yapay zeka destekli kod üretim uygulaması
+Kimi K2.5 API ile çalışır
+
+Author: Senin Adın
+Date: 2026-03-28
+Version: 1.0.0
+"""
+
+import streamlit as st
+import requests
+import os
+
+# ... geri kalan kod buraya ...
